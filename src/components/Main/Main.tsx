b0b78b772032da446159905a4501e9c6003c3905
Yasser-Shehab <yassershehab9@gmail.com>
2023-02-02 18:07:23 +0200

    Created Search Input with button

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,14 +1,15 @@
 import './Main.scss';
+import searchIcon from '../../assets/Images/SearchIcon.svg';
 
 const Main = () => {
   return (
     <section className='main-container'>
-      <h4>تتبع شحنتك</h4>
+      <h4 className='header-section'>تتبع شحنتك</h4>
       <div className='search-container'>
-        <span>
-          <input type='text' placeholder='رقم التتبع' />
-        </span>
-        <span></span>
+        <input type='text' placeholder='رقم التتبع' className='search-input' />
+        <button className='search-button'>
+          <img src={searchIcon} alt='Search Icon' />
+        </button>
       </div>
     </section>
   );
