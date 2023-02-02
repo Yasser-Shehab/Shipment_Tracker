b398b461061f91ff3d55faa4d70058a44abd869a
Yasser-Shehab <yassershehab9@gmail.com>
2023-02-02 17:39:02 +0200

    Added Main Section (WIP)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import svg from './assets/Images/LogoAr.svg';
+import Main from './components/Main/Main';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className='container'>
       <Navbar />
+      <Main />
     </div>
   );
 }
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,17 @@
 import './Main.scss';
 
 const Main = () => {
-  return <section className='main-container'>Main</section>;
+  return (
+    <section className='main-container'>
+      <h4>تتبع شحنتك</h4>
+      <div className='search-container'>
+        <span>
+          <input type='text' placeholder='رقم التتبع' />
+        </span>
+        <span></span>
+      </div>
+    </section>
+  );
 };
 
 export default Main;
