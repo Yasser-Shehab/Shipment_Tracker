5eacca170a086aee0b5a2216d122c6d713e51580
Yasser-Shehab <yassershehab9@gmail.com>
2023-02-03 19:00:36 +0200

    LanguageChange added to shared

---
+++ src/Shared/LanguageChange/LanguageChange.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import type { RootState } from '../../store/store';
+import { Arabic, English } from '../../data/translate.js';
+import { setLanguage } from '../../store/language';
+import arrow from '../../assets/Images/SmallArrow.svg';
+
+const LanguageChange = () => {
+  const [showLang, setShowLang] = useState(false);
+  const dispatch = useDispatch();
+  const lang = useSelector((state: RootState) => state.language.language);
+
+  const handleLanguageChange = () => {
+    if (lang === 'en') {
+      dispatch(setLanguage('ar'));
+    } else {
+      dispatch(setLanguage('en'));
+    }
+  };
+  const handleShowLang = () => {
+    setShowLang(!showLang);
+  };
+
+  return (
+    <div className='language'>
+      <div className='language-change' onClick={handleShowLang}>
+        <p>{lang === 'en' ? English['en'] : Arabic['ar']}</p>
+        <span>
+          <img src={arrow} alt='arrow' className='arrow-image' />
+        </span>
+      </div>
+      {showLang ? (
+        <ul>
+          <li onClick={handleLanguageChange}>
+            {lang === 'en' ? <p>English</p> : <p>اللغة العربية</p>}
+          </li>
+          <li onClick={handleLanguageChange}>
+            {lang === 'en' ? <p>Arabic</p> : <p>اللغة الانجليزية</p>}
+          </li>
+        </ul>
+      ) : null}
+    </div>
+  );
+};
+
+export default LanguageChange;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,29 +1,9 @@
 import React, { useState } from 'react';
 import './Navbar.scss';
 import LogoAr from '../../assets/Images/LogoAr.svg';
-import type { RootState } from '../../store/store';
-import { useSelector, useDispatch } from 'react-redux';
-import { setLanguage } from '../../store/language';
-import arrow from '../../assets/Images/SmallArrow.svg';
-import { Arabic, English } from '../../data/translate.js';
+import LanguageChange from '../../Shared/LanguageChange/LanguageChange';
 
 const Navbar = () => {
-  const [showLang, setShowLang] = useState(false);
-  const dispatch = useDispatch();
-  const lang = useSelector((state: RootState) => state.language.language);
-
-  const handleLanguageChange = () => {
-    if (lang === 'en') {
-      dispatch(setLanguage('ar'));
-    } else {
-      dispatch(setLanguage('en'));
-    }
-  };
-
-  const handleShowLang = () => {
-    setShowLang(!showLang);
-  };
-
   return (
     <nav className='nav'>
       <div>
@@ -31,25 +11,7 @@ const Navbar = () => {
           <img src={LogoAr} alt='Bosta Logo' className='logo' />
         </div>
       </div>
-
-      <div className='language'>
-        <div className='language-change' onClick={handleShowLang}>
-          <p>{lang === 'en' ? English['en'] : Arabic['ar']}</p>
-          <span>
-            <img src={arrow} alt='arrow' className='arrow-image' />
-          </span>
-        </div>
-        {showLang ? (
-          <ul>
-            <li onClick={handleLanguageChange}>
-              {lang === 'en' ? <p>English</p> : <p>اللغة العربية</p>}
-            </li>
-            <li onClick={handleLanguageChange}>
-              {lang === 'en' ? <p>Arabic</p> : <p>اللغة الانجليزية</p>}
-            </li>
-          </ul>
-        ) : null}
-      </div>
+      <LanguageChange />
     </nav>
   );
 };
